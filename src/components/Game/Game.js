82111f55ca90c83facf9275eff99a3bebf9c09dc
younessbennaj <youness.bennaj@gmail.com>
2024-05-18 14:06:38 +0200

    feat: add game logic

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,43 +4,31 @@ import { range, sample } from "../../utils";
 import { WORDS } from "../../data";
 
 import GuessInput from "../GuessInput";
-import { NUM_OF_GUESSES_ALLOWED, MAX_LENGTH_WORD } from "../../constants";
+import Guess from "../Guess";
+
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
-function Guess({ word }) {
-  return (
-    <p className="guess">
-      {range(0, MAX_LENGTH_WORD).map((id) => {
-        return (
-          <span className="cell" key={id}>
-            {word && word[id]}
-          </span>
-        );
-      })}
-    </p>
-  );
-}
-
 function Game() {
-  const [words, setWords] = useState([]);
+  const [guesses, setGuesses] = useState([]);
   function onWordSubmit(word) {
-    const newWords = [...words, word];
-    setWords(newWords);
+    const newWords = [...guesses, word];
+    setGuesses(newWords);
   }
   return (
     <>
       <div className="guess-results">
         {range(0, NUM_OF_GUESSES_ALLOWED).map((id) => {
-          return <Guess key={id} word={words[id]} />;
+          return <Guess answer={answer} guess={guesses[id]} key={id} />;
         })}
       </div>
       <GuessInput onWordSubmit={onWordSubmit} />
       <div className="guess-results">
-        {words.map((word) => {
+        {guesses.map((word) => {
           return <p key={word}>{word}</p>;
         })}
       </div>
